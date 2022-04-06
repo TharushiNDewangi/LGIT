ea80c21610961d3930e5cc277fec0f14d1751eda
Dell <navodyathenuwara81@gmail.com>
2022-04-06 21:43:25 +0530

    Notes

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,38 +1,13 @@
 // /**
 //  * created by IT19129518 Rathnayake R.M.D.M
 //  */
-// class Note {
-//   late String id;
-//   late String title;
-//   late String content;
 
-//   Note(
-//       {required this.id,
-//       required this.title,
-//       required this.content,});
-
-//   /**
-//    * From tutorial
-//    * This method used to convert to a object file
-//    */
-//   Map<String, dynamic> toConvertJson() => {
-//         "id": id,
-//         "question": title,
-//         "content": content,
-//       };
-
-//   Note.fromSnapshot(snapshot)
-//       : id = snapshot.data()['id'],
-//         title = snapshot.data()['title'],
-//         content = snapshot.data()['content']
-// }
-/*Created by - IT19246024 - Warnakulasuriya D.A*/
-class Blog {
+class Note {
   late String id;
   late String title;
   late String content;
 
-  Blog({
+  Note({
     required this.id,
     required this.title,
     required this.content,
@@ -42,7 +17,7 @@ class Blog {
   Map<String, dynamic> toConvertJson() =>
       {"id": id, "title": title, "content": content};
 
-  Blog.fromSnapshot(snapshot)
+  Note.fromSnapshot(snapshot)
       : id = snapshot.data()['id'],
         title = snapshot.data()['title'],
         content = snapshot.data()['content'];
